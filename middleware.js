3f499036288fa821b34448b2828d89174f503e31
sufian008 <m.a.sufian008@gmail.com>
2025-05-05 22:44:40 +0600

    Flash

--- middleware.js
+++ middleware.js
@@ -2,6 +2,8 @@ module.exports.isLoggedIn = (req, res, next) =>{
     if(!req.isAuthenticated()){
         req.flash("success", "You must be Logged in to create Listing");
        return  res.redirect("/login");
+   }else{
+    req.flash("Failed", "You must be Logged in to create Listing");
    }
        
    next();
